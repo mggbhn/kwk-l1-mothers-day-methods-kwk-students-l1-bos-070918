a6e8637bd9b261cb4b6d6c47a6d56bf991f88ef5
mggbhn <maggiebswim@gmail.com>
2018-07-10 17:22:22 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,5 +1,5 @@
 def holiday_greeting(theirname = "Mom", yourname = "Your Favorite Child", holiday = "Mother's Day")
-  "Happy #{holiday},#{theirname}! - From #{yourname}"
+  "Happy #{holiday}, #{theirname}! - From #{yourname}"
 end
 
 puts holiday_greeting
